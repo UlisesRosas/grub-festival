66060e3a875fdcb7dfd387ed1d6bc0479b5d3c93
ulisMac <rosas_u@ymail.com>
2022-05-03 01:49:45 -0700

    aded bundle for images and js files in js foleder

--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,67 @@
 const path = require('path');
 // to inport wwebpack properties and methods
 const webpack = require("webpack");
+// this import is to analize the bundles
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 // exports basic configuration for wbpack
 module.exports = {
     // where webpack looks to start buildinf the module
-    entry: './assets/js/script.js',
-    output: {
-    // tells webpack where the files are going to go
-    path: path.resolve(__dirname, 'dist'),
-    // this file will br in the dist directory
-    filename: 'main.bundle.js'
+    // entry points 
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
     },
+    output: {
+
+        // this file will br in the dist directory
+        filename: '[name].bundle.js',
+        // tells webpack where the files are going to go
+        path: __dirname + '/dist'
+    },
+
+    module: {
+        rules: [
+          {
+            test: /\.jpg$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  esModule: false,
+                  name(file) {
+                    return '[path][name].[ext]';
+                  },
+                  publicPath: function(url) {
+                    return url.replace('../', '/assets/');
+                  }
+                }
+              },
+              {
+                loader: 'image-webpack-loader'
+              }
+            ]
+          }
+        ]
+      },
+    
+
     // plugins helps web pack know what to do with libraries that use global variables scuh as '$' with JQuery
     // if not done there will be errors where thses global vars are not recognized
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
-          }),
+        }),
+        new BundleAnalyzerPlugin({
+            //   static will show a report of the bundle in the browser
+            // ir will come in the form of a HTMl file in the dist directory
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
     ],
+
     // this gets changed when we want our code to be minified to 'production'
     mode: 'development'
 };
